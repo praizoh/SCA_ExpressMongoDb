9ef6843b097f633dc326217c58720eabd559ba7a
praizoh <oremei.akande@gmail.com>
2020-07-01 12:24:21 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+let express = require('express')
+let app = express()
+app.use(express.static('public'))
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, ()=>console.info(`Server has started on ${PORT}`))
