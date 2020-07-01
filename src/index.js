015b4390110cc7a36a590453624a953c3c301535
praizoh <oremei.akande@gmail.com>
2020-07-01 14:48:56 +0100

    'update'

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 let express = require('express')
 let app = express()
-app.use(express.static('public'))
 
+const path = require('path')
+let personRoute = require('./routes/person')
+
+app.use((req,res, next)=>{
+    console.log(`${new Date().toString()} => ${req.originalUrl}`)
+    next()
+})
+
+app.use(personRoute)
+app.use(express.static('public'))
+app.use((req,res)=>{
+    res.status(404).send('We think you are lost')
+})
+app.use((err, req,res, next)=>{
+    console.error(err.stack)
+    res.sendFile(path.join(__dirname, '../public/500.html'))
+})
 const PORT = process.env.PORT || 3000
 app.listen(PORT, ()=>console.info(`Server has started on ${PORT}`))
